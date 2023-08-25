12b4e3310a786955f2c213ac7194b57a1453b79a
Chris <chrisphilholt@gmail.com>
2023-08-25 16:27:55 +0100

    trying to fix build errors for netlfiy build

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
     return (
         <section className="relative h-screen w-screen flex flex-col items-center justify-center">
             <h1>WORK</h1>
-            <h1 className={headingStyle}>Projects I've made</h1>
+            <h1 className={headingStyle}>Projects I&apos;ve made</h1>
             <div className="w-3/4 h-1/2 bg-black"></div>
         </section >
     )

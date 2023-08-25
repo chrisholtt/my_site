a9f0f3974668759cff3c60152a2318722f6cb988
Chris <chrisphilholt@gmail.com>
2023-08-25 16:41:32 +0100

    replace img tags with IMage tags from nextjs

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import React, { useEffect, useState } from 'react'
 import { skills, headingStyle, subHeadingStyle } from "@/utils/utils";
 import Tooltip from '@mui/material/Tooltip';
+import Image from "next/image";
 
 
 
@@ -13,7 +14,7 @@ export default function Hero() {
         return skills.map(skill => (
             <Tooltip title={skill.title} key={skill.title}>
                 <div className="w-40 h-40 bg-white bg-opacity-70 rounded-md flex flex-col justify-center items-center text-center transition-transform transform hover:shadow-md cursor-pointer" id={skill.title}>
-                    <img src={skill.source} alt="skill-icon" className="w-20" />
+                    <Image src={skill.source} alt="skill-icon" className="w-20" />
                 </div>
             </Tooltip>
         ))
@@ -24,34 +25,34 @@ export default function Hero() {
             <div className="flex flex-col justify-center items-center">
                 <div className='flex flex-row space-x-1'>
                     <Tooltip title={skills[0].title}>
-                        <img src={skills[0].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[0].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[1].title}>
-                        <img src={skills[1].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[1].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[2].title}>
-                        <img src={skills[2].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[2].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[3].title}>
-                        <img src={skills[3].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[3].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[4].title}>
-                        <img src={skills[4].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[4].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
 
                 </div>
                 <div className='flex flex-row space-x-1'>
                     <Tooltip title={skills[5].title}>
-                        <img src={skills[5].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[5].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[6].title}>
-                        <img src={skills[6].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[6].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[7].title}>
-                        <img src={skills[7].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[7].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                     <Tooltip title={skills[8].title}>
-                        <img src={skills[8].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
+                        <Image src={skills[8].source} alt="" className="w-20 h-20 max-w-20 max-h-20" />
                     </Tooltip>
                 </div>
             </div>

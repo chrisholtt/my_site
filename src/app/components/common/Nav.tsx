b3a1228bbf0b51972cef47880772bcd18ad10a98
Chris <chrisphilholt@gmail.com>
2023-10-30 15:02:06 +0000

    added default avatar icon and isabled contact inputs w/ no session

--- src/app/components/common/Nav.tsx
+++ src/app/components/common/Nav.tsx
@@ -1,8 +1,20 @@
 "use client"
 import Link from 'next/link';
 import SignInButton from './SignInButton';
+import { Button } from '@mui/material';
+
 
 export default function Nav() {
+    const CvDownload = () => {
+        return (
+            <Button className='border-stone-800 text-stone-300 hover:border-stone-600' variant="outlined">
+                <a href="/misc/ChrisHoltCV.pdf" download="Chris-Holts-CV.pdf" className='flex'>
+                    <h1>RESUME</h1>
+                </a>
+            </Button>
+        )
+    }
+
     return (
         <nav className='fixed dark:bg-black bg-zinc-50 w-screen flex flex-row justify-between px-2 border-b border dark:border-stone-800 light:border-zinc-200' style={{ zIndex: 10 }}>
             <ul className="flex flex-row items-center">
@@ -13,6 +25,9 @@ export default function Nav() {
                 </li>
             </ul>
             <ul className='flex flex-row items-center gap-2'>
+                <li>
+                    <CvDownload />
+                </li>
                 <li >
                     <SignInButton />
                 </li>

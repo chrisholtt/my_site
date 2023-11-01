a63167570b1a96c9f268899dd33aaec9645c83b7
Chris <chrisphilholt@gmail.com>
2023-11-01 18:49:29 +0000

    fix up

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -34,7 +34,7 @@ const Star = () => {
 export default function Hero() {
 
     return (
-        <section className="relative z-10 h-screen w-screen flex items-center justify-center">
+        <section className="relative h-screen w-screen flex items-center justify-center">
             <div className="flex flex-col justify-between w-auto items-center text-center space-y-6 px-10">
                 <h1 className={`${titleStyle} drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}>Creative Develope<span className="bg-transparent animate-pulse">r</span></h1>
                 <h2 className={`${subHeadingStyle} `}>Software developer from Edinburgh, Scotland. I like to make cool things and experiment with new technologies. Feel free to scroll down and discover more about me and my work.</h2>

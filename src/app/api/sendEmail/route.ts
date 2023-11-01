c651a17ee660de266b871879bd7a99468178c00f
Chris <chrisphilholt@gmail.com>
2023-11-01 18:36:55 +0000

    fix up

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -13,15 +13,14 @@ export async function POST(req: Request, res: Response) {
             message: message,
             subject: subject
         }
-        emailjs.init(process.env.NEXT_PUBLIC_EMAILJS_ID);
-        const resEmail = await emailjs.sendForm(process.env.NEXT_PUBLIC_EMAILJS_SERVICE, emailData, process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE)
-            .then((result) => {
-                console.log("result text: " + result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        console.log(resEmail)
-        return NextResponse.json(resEmail)
+        // const resEmail = await emailjs.sendForm(process.env.NEXT_PUBLIC_EMAILJS_SERVICE, emailData, process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE)
+        //     .then((result) => {
+        //         console.log("result text: " + result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
+        // console.log(resEmail)
+        return NextResponse.json("resEmail")
 
     } catch (err) {
         return NextResponse.json({ message: 'Internal server error' })

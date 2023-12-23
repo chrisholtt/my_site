7b1f1ce1cf24ae6e8e38024493a41f85b26cd512
Chris <chrisphilholt@gmail.com>
2023-12-23 11:04:01 +0000

    added clsx

--- src/app/components/common/Nav.tsx
+++ src/app/components/common/Nav.tsx
@@ -3,10 +3,15 @@ import Link from 'next/link';
 import SignInButton from './SignInButton';
 import Image from 'next/image';
 import { useTheme } from "next-themes";
+import clsx from "clsx";
 
 
 export default function Nav() {
     const { theme, setTheme } = useTheme();
+
+    const imageSrc = clsx(
+        theme == 'dark' ? '/images/logo_black.svg' : '/images/logo_white.svg'
+    )
 
 
     return (
@@ -14,7 +19,7 @@ export default function Nav() {
             <ul className="flex flex-row items-center">
                 <li className="p-1 cursor-pointer font-roboto text-3xl align-middle justify-center">
                     <Link href='/' className='flex flex-row'>
-                        <Image src={`/images/logo_${theme != 'dark' ? 'black' : 'white'}.svg`} width={30} height={30} priority alt="star" />
+                        <Image src={imageSrc} width={30} height={30} priority alt="star" />
                     </Link>
                 </li>
             </ul>

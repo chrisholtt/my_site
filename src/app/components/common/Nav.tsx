4a23df7a1ae146d66e836f4f0c0a7cb0fdd68377
Chris <chrisphilholt@gmail.com>
2023-12-23 11:17:57 +0000

    updated nav style]

--- src/app/components/common/Nav.tsx
+++ src/app/components/common/Nav.tsx
@@ -4,21 +4,24 @@ import SignInButton from './SignInButton';
 import Image from 'next/image';
 import { useTheme } from "next-themes";
 import clsx from "clsx";
-
+import React, { useEffect, useState } from 'react';
 
 export default function Nav() {
     const { theme, setTheme } = useTheme();
+    const [imageSrc, setImageSrc] = useState<string>('/images/logo_white.svg')
 
-    const imageSrc = clsx(
-        theme == 'dark' ? '/images/logo_black.svg' : '/images/logo_white.svg'
-    )
+    useEffect(() => {
+        setImageSrc(theme === 'dark' ? '/images/logo_white.svg' : '/images/logo_black.svg')
+    }, [theme])
+
+
 
 
     return (
         <nav className='fixed frosted w-screen flex flex-row justify-between px-2 border-b border dark:border-stone-800 light:border-zinc-200' style={{ zIndex: 10 }}>
             <ul className="flex flex-row items-center">
-                <li className="p-1 cursor-pointer font-roboto text-3xl align-middle justify-center">
-                    <Link href='/' className='flex flex-row'>
+                <li className="p-1 hover:bg-neutral-600 hover:bg-opacity-30 transition rounded-full">
+                    <Link href='/'>
                         <Image src={imageSrc} width={30} height={30} priority alt="star" />
                     </Link>
                 </li>
